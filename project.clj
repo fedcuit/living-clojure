6cd4e11873c50bb083ee2605e0032984f49d040b
Erdong Feng <edfeng@thoughtworks.com>
2018-12-27 22:20:24 +0800

    change project name

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject living-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Living clojure"
+  :url "https://github.com/fedcuit/living-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot living-clojure.core
   :target-path "target/%s"
